070008841ffbd6118d741c56425922276d3b072c
Matt Castillo <mattcastillo2010@gmail.com>
2021-01-23 21:50:19 -0600

    Added dependencies

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://thawing-headland-83916.herokuapp.com/',
+  API_ENDPOINT: 'https://thawing-headland-83916.herokuapp.com/api',
   API_KEY: process.env.REACT_APP_API_KEY,
 }
